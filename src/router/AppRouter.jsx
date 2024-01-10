6596a5118ebbc441adc0b01761322a78301067a6
Dalmiro Zantleifer <dzantleifer@sofredigital.com.ar>
2024-01-10 14:06:09 -0300

    rutas privadas rutas públicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,30 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { HerosRoutes } from "../heros";
 import { LoginPage } from "../auth";
+import { PrivateRoutes } from "./";
+import { PublicRoutes } from "./";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HerosRoutes />} />
+        <Route
+          path="login"
+          element={
+            <PublicRoutes>
+              <LoginPage />
+            </PublicRoutes>
+          }
+        />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoutes>
+              <HerosRoutes />
+            </PrivateRoutes>
+          }
+        />
       </Routes>
     </>
   );
